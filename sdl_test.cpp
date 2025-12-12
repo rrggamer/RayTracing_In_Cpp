161487ff2bf5218706cdd7b1897085cf995444cc
Pha <phakaewpra@gmail.com>
2025-12-12 16:18:34 +0700

    add draw function

--- sdl_test.cpp
+++ sdl_test.cpp
@@ -12,6 +12,7 @@ struct Circle
     float x;
     float y;
     float radius;
+    Uint32 color;
 
     /* Circle(float X_, float Y_, float radius_){
         this->x = X_;
@@ -19,20 +20,56 @@ struct Circle
         this->radius = radius_;
     }*/
 
-    Circle(float X_, float Y_, float radius_)
-        : x(X_), y(Y_), radius(radius_){}
+    Circle(float X_, float Y_, float radius_, Uint32  color_)
+        : x(X_), y(Y_), radius(radius_), color(color_){}
 
 };
 
 
-void drawCircle(SDL_Surface *surface, Circle& circle ){
+// void drawCircle(SDL_Surface *surface, Circle& circle ){
     
-    float x = circle.x;
-    float y = circle.y;
-    SDL_Rect dstRect = {x,y,circle.radius,circle.radius};
-    SDL_FillRect(surface, &dstRect, WHITE_COLOR);
-    printf("x: %f, y: %f\n",x,y);
-    
+//     double radius_squared = (circle.radius) * (circle.radius);
+
+//     for(int x = circle.x - circle.radius; x <= circle.x+circle.radius; x++){
+//         for(double y = circle.y - circle.radius; y <= circle.y+circle.radius; y++){
+
+//             double distance_squared = (x-circle.x)*(x-circle.x) + (y-circle.y) * (y-circle.y);
+//             if( distance_squared < radius_squared){
+
+//                 SDL_Rect pixel = {x,y,1,1};
+//                 SDL_FillRect(surface, &pixel, circle.color);
+
+//             }
+
+//         } 
+
+//     }
+// }
+
+static inline void putPixel(SDL_Surface* s, int x, int y, Uint32 color) {
+    if ((unsigned)x >= (unsigned)s->w || (unsigned)y >= (unsigned)s->h) return;
+    Uint32* pixels = (Uint32*)s->pixels;
+    int pitchPixels = s->pitch / 4;           // 4 bytes per pixel (ARGB8888/RGBA8888)
+    pixels[y * pitchPixels + x] = color;      // 2D -> 1D
+}
+
+
+void drawFilledCircle(SDL_Surface* s, int cx, int cy, int r, Uint32 color) {
+    if (SDL_MUSTLOCK(s)) SDL_LockSurface(s);
+
+    int r2 = r * r;
+
+    for (int y = cy - r; y <= cy + r; ++y) {
+        int dy = y - cy;
+        for (int x = cx - r; x <= cx + r; ++x) {
+            int dx = x - cx;
+            if (dx*dx + dy*dy <= r2) {
+                putPixel(s, x, y, color);
+            }
+        }
+    }
+
+    if (SDL_MUSTLOCK(s)) SDL_UnlockSurface(s);
 }
 
 
@@ -42,10 +79,11 @@ int main(){
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window* win = SDL_CreateWindow("Window",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,640,480,SDL_WINDOW_SHOWN);
     SDL_Surface* winSurface = SDL_GetWindowSurface(win);
+    
 
     SDL_FillRect(winSurface, NULL, BLACK_COLOR);
     
-    Circle circle(10,50,80);
+    Circle circle(200, 200, 80, WHITE_COLOR);
 
     int app_is_running = 1;
     while (app_is_running){
@@ -59,7 +97,7 @@ int main(){
             }
 
         }
-        drawCircle(winSurface,circle);
+        drawFilledCircle(winSurface, (int)circle.x, (int)circle.y, (int)circle.radius, WHITE_COLOR);
         SDL_UpdateWindowSurface(win);
     }
 
